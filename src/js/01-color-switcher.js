f6c267c272a1e9475e507c3f85af6af148c2ffea
Roman <pika1roman@gmail.com>
2023-03-12 21:33:32 +0100

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
 
+const CHANGE_BG_COLOR_DELAY = 1000;
+
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop'),
+  bodyElem: document.querySelector('body'),
+}
+
+let intervalId = null;
+
+refs.startButton.addEventListener('click', onStartButtonClick);
+refs.stopButton.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick() {
+  refs.startButton.disabled = true;
+  intervalId = setInterval(() => {
+    const bgColor = getRandomHexColor();
+    refs.bodyElem.style.backgroundColor = bgColor;
+  }, CHANGE_BG_COLOR_DELAY);
+}
+
+function onStopButtonClick() {
+  refs.startButton.disabled = false;
+  clearInterval(intervalId);
+  // intervalId = null;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+
+
+
+
+
